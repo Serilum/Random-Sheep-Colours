e677bc5d820cababef72a2950d6f0b627840b7b6
Rick South <rick@natamus.com>
2023-12-12 00:22:55 +0100

    Random Sheep Colours source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/randomsheepcolours/neoforge/events/NeoForgeSheepEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.randomsheepcolours.neoforge.events;
+
+import com.natamus.randomsheepcolours.events.SheepEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeSheepEvent {
+	@SubscribeEvent
+	public static void onSheepSpawn(EntityJoinLevelEvent e) {
+		SheepEvent.onSheepSpawn(e.getLevel(), e.getEntity());
+	}
+}
