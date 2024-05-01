50caea2ed3bba318bd3a6bd04f51ec3ca1a5b3c2
Rick South <rick@natamus.com>
2024-05-01 16:32:46 +0200

    Random Sheep Colours source code for 1.20.6.

---
+++ Forge/src/main/java/com/natamus/randomsheepcolours/ModForge.java
@@ -0,0 +1,36 @@
+package com.natamus.randomsheepcolours;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.randomsheepcolours.forge.config.IntegrateForgeConfig;
+import com.natamus.randomsheepcolours.forge.events.ForgeSheepEvent;
+import com.natamus.randomsheepcolours.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgeSheepEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
