0bb5fa90142250262f375870d6889a01df67f561
Rick South <rick@natamus.com>
2024-06-14 05:15:46 +0200

    Random Sheep Colours source code for 1.21.0.

---
+++ NeoForge/src/main/java/com/natamus/randomsheepcolours/ModNeoForge.java
@@ -0,0 +1,34 @@
+package com.natamus.randomsheepcolours;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.randomsheepcolours.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.randomsheepcolours.neoforge.events.NeoForgeSheepEvent;
+import com.natamus.randomsheepcolours.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeSheepEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
