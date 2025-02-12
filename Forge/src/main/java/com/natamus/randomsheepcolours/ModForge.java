1512eaa47726c5b1da819612ef91349659e3176f
Rick South <rick@natamus.com>
2025-02-12 23:18:04 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/randomsheepcolours/ModForge.java
+++ Forge/src/main/java/com/natamus/randomsheepcolours/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new ForgeSheepEvent());
+    	MinecraftForge.EVENT_BUS.register(ForgeSheepEvent.class);
 	}
 
 	private static void setGlobalConstants() {
